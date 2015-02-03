41f4f697bd733be1ff9a0f7db85e157c9f4fb06b
gimoz71 <gianluca.monti@ecoseekr.com>
2015-02-03 17:29:57 +0100

    test

--- conf/www.nexsia.net.php
+++ conf/www.nexsia.net.php
@@ -1,6 +1,8 @@
 <?php 
 
-$server_pth = getenv("DOCUMENT_ROOT");
-$server_url = "http://www.nexsia.net/";
-
+	$server_pth = getenv("DOCUMENT_ROOT");
+	$server_url = "http://www.nexsia.net/";
+	echo $server_pth;
+	echo $server_url;
+	
 ?>
