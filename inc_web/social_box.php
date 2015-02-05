6a7c47cc140f31d5dd0cb93f11023e4001a5df1c
gimoz71 <gianluca.monti@ecoseekr.com>
2015-02-06 00:38:06 +0100

    twitter test

--- inc_web/social_box.php
+++ inc_web/social_box.php
@@ -1,35 +1,6 @@
 <div style="float: left; margin: 20px 10px 0;">
-	<script src="http://widgets.twimg.com/j/2/widget.js"></script>
-	<script>
-		new TWTR.Widget({
-		  version: 2,
-		  type: 'profile',
-		  rpp: 3,
-		  interval: 6000,
-		  width: 430,
-		  height: 200,
-		  theme: {
-			shell: {
-			  background: '#ECEEF3',
-			  color: '#666'
-			},
-			tweets: {
-			  background: '#ffffff',
-			  color: '#000000',
-			  links: '#599bde'
-			}
-		  },
-		  features: {
-			scrollbar: false,
-			loop: false,
-			live: false,
-			hashtags: true,
-			timestamp: true,
-			avatars: true,
-			behavior: 'all'
-		  }
-		}).render().setUser('admizesrl').start();
-    </script>
+    <a class="twitter-timeline" href="https://twitter.com/Nexsiadv" data-widget-id="563481535807246336">Tweet di @Nexsiadv</a>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </div>
 
 <div style="float: left; margin: 20px 0 0 0;">
