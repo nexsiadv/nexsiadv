ddd9e350e7ff2473f864713a882fa3007086b578
gimoz71 <gianluca.monti@ecoseekr.com>
2015-02-06 00:41:31 +0100

    twitter test 2

--- inc_web/social_box.php
+++ inc_web/social_box.php
@@ -1,5 +1,5 @@
 <div style="float: left; margin: 20px 10px 0;">
-    <a class="twitter-timeline" href="https://twitter.com/Nexsiadv" data-widget-id="563481535807246336">Tweet di @Nexsiadv</a>
+    <a class="twitter-timeline" href="https://twitter.com/Nexsiadv" data-widget-id="563481535807246336" width="440" height="250">Tweet di @Nexsiadv</a>
     <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </div>
 
