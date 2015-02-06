5ad94721b7a05bc1400f908509606b127113d1e7
gimoz71 <gianluca.monti@ecoseekr.com>
2015-02-06 21:48:31 +0100

    fix char advertising

--- web-advertising/inc_web/a-cosa-serve.php
+++ web-advertising/inc_web/a-cosa-serve.php
@@ -9,7 +9,7 @@
     <img src="<?php echo $server_url; ?>images/web_advertising_2.png" alt="schema del processo creativo dell'advertising" width="390" height="367" style="float: left; margin-right: 20px; position: relative; top: -50px;">
     <h2>Cosa possiamo fare per te...</h2>
     <h3><strong>Nuovi concetti e strategie pubblicitarie per incrementare la tua efficacia online! </strong>Concorsi a premi, sondaggi, lancio di prodotti o contesti digitali oppure  semplice gestione e pianificazione della tua campagna pubblicitaria online.</h3>
-    <h3>Selezioniamo <strong>i migliori e pi&amp;ugrave; performanti siti web adatti ad esprimere i tuoi messaggi pubblicitari</strong>, scegliamo gli argomenti più affini e sviluppiamo le tue creativit&agrave; <em>(annunci costituiti da immagini o testi)</em> e le posizioniamo online per attirare chi ti st&agrave; cercando.</h3>
+    <h3>Selezioniamo <strong>i migliori e pi&ugrave; performanti siti web adatti ad esprimere i tuoi messaggi pubblicitari</strong>, scegliamo gli argomenti più affini e sviluppiamo le tue creativit&agrave; <em>(annunci costituiti da immagini o testi)</em> e le posizioniamo online per attirare chi ti st&agrave; cercando.</h3>
     <h3>Altre alla <strong>pianificazione della campagna pubblicitaria online</strong>, costruiamo un PIANO D'AZIONE ed una LINEA DI BUDGET  per il tuo investimento nell'advertising online e mettiamo in evidenza i punti di controllo, gli obiettivi, le strategie, i modi ed i tempi di realizzazione.</h3>
     <h3><strong> Monitoriamo costantemente le performance della tua campagna di advertising online ed  operiamo per il raggiungimento del ROI <em>(ritorno dall'investimento)</em>.</strong></h3>
     <?php @include($server_pth."/inc_web/request_button.php"); ?>
